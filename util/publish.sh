56464eaa33f3431c96000bda1523b0a2f2b92bd6
Mitya Teryaew <mitya.teryaew@gmail.com>
2017-08-06 23:07:42 +0300

    chore(*): update publishing

--- util/publish.sh
+++ util/publish.sh
@@ -8,6 +8,8 @@ git config user.email "travis@travis-ci.org"
 git remote add upstream "https://$GH_TOKEN@github.com/teryaew/teryaew.earth.git"
 git fetch -q upstream && git reset -q upstream/gh-pages
 
+elm make ./src/index.elm
+
 git add ./index.html
 git commit -q -m "chore(*): update site by travis build $TRAVIS_BUILD_NUMBER"
 git push -q upstream HEAD:gh-pages
